ff5bf0248f2fae783e395ac4b1ba36acff487070
Weera Wu <weera@oozou.com>
2015-12-10 19:16:55 +0700

    Initial commit

---
+++ server.rb
@@ -0,0 +1,57 @@
+require 'sinatra'
+require 'securerandom'
+require 'digest/sha1'
+require_relative 'message'
+
+configure :production do
+  set :token, ENV['TOKEN'] || SecureRandom.urlsafe_base64
+  puts "Token set to #{settings.token}"
+end
+
+helpers do
+  # http://admin.wechat.com/wiki/index.php?title=Message_Authentication
+  def check_signature
+    nonce     = params[:nonce]
+    signature = params[:signature]
+    timestamp = params[:timestamp]
+
+    tmp_arr = [settings.token, timestamp, nonce].compact.sort
+    tmp_str = tmp_arr.join
+    tmp_str = Digest::SHA1.hexdigest(tmp_str)
+
+    tmp_str == signature
+  end
+end
+
+before do
+  halt 403 if settings.production? && !check_signature
+  logger.debug request.body.read
+end
+
+after do
+  logger.debug body
+end
+
+# http://admin.wechat.com/wiki/index.php?title=Getting_Started
+get '/' do
+  body params[:echostr]
+end
+
+# http://admin.wechat.com/wiki/index.php?title=Common_Messages
+post '/' do
+  request.body.rewind
+  incoming = Message.parse(request.body.read)
+
+  outgoing = Message.new(
+    sender:    incoming.recipient,
+    recipient: incoming.sender,
+    sent_at:   Time.now.utc,
+    content:   "I don't understand what you said"
+  )
+
+  body outgoing.to_xml
+end
+
+error NotImplementedError do
+  body ''
+end
